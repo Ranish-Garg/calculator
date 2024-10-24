9e4221875529f7a328905f96b655c11c1891991b
24je0682 <24je0682@iitism.ac.in>
2024-10-24 10:16:27 +0530

    Inverse imlemented

--- calculator.c
+++ calculator.c
@@ -74,7 +74,7 @@ int main() {
             case 'i':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("the result of i of %d is .2%f\n",a,1/a);
                 break;
             default:
                 printf("Invalid operation. Please try again.\n");
